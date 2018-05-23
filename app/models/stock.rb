0ea91c0947c8ddb9fd896b83b621893eb53347be
zacky <kic.yuuki@gmail.com>
2018-05-23 19:10:36 -0400

    add stocks routes

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,17 @@
 class Stock < ActiveRecord::Base
+  
+  # 株式情報を取得する処理
+  # ticker_symbol - 名前(ex. "GOOG"など)
+  def self.new_from_lookup(ticker_symbol)
+    
+    # 株式情報取得
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    
+    # TODO: 検索失敗時の対応が必要
+    # インスタンスを返す
+    new(name: looked_up_stock.company_name,
+        ticker: looked_up_stock.symbol,
+        last_price: looked_up_stock.latest_price)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
