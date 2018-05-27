edeb617e3d76a105630db092df701f212113de4b
zacky <kic.yuuki@gmail.com>
2018-05-27 18:43:17 -0400

    nil and exeption catch logic add

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,14 +4,21 @@ class Stock < ActiveRecord::Base
   # ticker_symbol - 名前(ex. "GOOG"など)
   def self.new_from_lookup(ticker_symbol)
     
-    # 株式情報取得
-    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    begin
+      
+      # 株式情報取得
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+
+      # TODO: 検索失敗時の対応が必要
+      # インスタンスを返す
+      new(name: looked_up_stock.company_name,
+          ticker: looked_up_stock.symbol,
+          last_price: looked_up_stock.latest_price)
+      
+    rescue Exception => e
+      return nil
+    end
     
-    # TODO: 検索失敗時の対応が必要
-    # インスタンスを返す
-    new(name: looked_up_stock.company_name,
-        ticker: looked_up_stock.symbol,
-        last_price: looked_up_stock.latest_price)
   end
   
 end
