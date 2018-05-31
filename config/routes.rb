882e0e3f98a51176f1ad27ab34c347b6ee5274d1
zacky <kic.yuuki@gmail.com>
2018-05-31 19:07:06 -0400

    add stock table,stock delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
